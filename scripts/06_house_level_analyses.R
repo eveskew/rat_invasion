a89a28f7bd6a2e9a82d4491b4895a6053d3bd735
Evan Eskew <eveskew@Evans-MacBook-Pro.local>
2023-10-31 15:12:41 -0700

    Updating house-level analyses

--- scripts/06_house_level_analyses.R
+++ scripts/06_house_level_analyses.R
@@ -10,27 +10,50 @@ library(lubridate)
 
 # Data on track-level trapping effort across all sites
 summary.trap.dat <- read_csv("data/clean/PREEMPT/trap_metadata.csv") %>%
-  select(-mastomys, -house_type)
+  mutate(
+    # visit 5 at Petema is coded with two dates two days apart, recode to have
+    # the same visit date
+    date_mod = ifelse(
+      site == "Petema" & as.character(date) == "2021-01-28",
+      "2021-01-26",
+      as.character(date)
+    ),
+    # visit 2 at Badala is coded in the incorrect year, recode
+    date_mod = ifelse(
+      site == "Badala" & visit == 2,
+      "2019-11-09",
+      date_mod
+    ),
+    date_mod = as.Date(date_mod)
+  ) %>%
+  select(-mastomys, -house_type, -date) %>%
+  rename(date = date_mod) %>%
+  relocate(date, everything())
+
 # Data on rodent captures across all sites
 capture.dat <- read_csv("data/clean/PREEMPT/PREEMPT_capture_data.csv")
-# Summarized data on rodent capture at all sites: needed for site-level
-# lat/long information
-agg <- read_csv("data/clean/PREEMPT/aggregated_PREEMPT_capture_data.csv")
 
 
 # Generate track-level data tables for Sierra Leone
 
-# Summarize capture data by track ID
+# Summarize capture data by site, visit, and track ID
 capture.dat.by.track <- capture.dat %>%
   group_by(site, visit, track_id) %>%
   summarize(
+    n_catch = n(),
     n_Mna = sum(sp == "Mna"),
-    n_Rra = sum(sp == "Rra")
+    n_Rra = sum(sp == "Rra"),
+    n_Mer = sum(sp == "Mer"),
+    n_Pda = sum(sp == "Pda"),
+    n_Pro = sum(sp == "Pro")
   ) %>%
   ungroup()
 
 sum(capture.dat.by.track$n_Mna)
 sum(capture.dat.by.track$n_Rra)
+sum(capture.dat.by.track$n_Mer)
+sum(capture.dat.by.track$n_Pda)
+sum(capture.dat.by.track$n_Pro)
 
 # Join summarized capture data in with track-level trap data
 track.level.captures <- summary.trap.dat %>%
@@ -40,16 +63,22 @@ track.level.captures <- summary.trap.dat %>%
   ) %>%
   mutate(
     n_Mna = ifelse(is.na(n_Mna), 0, n_Mna),
-    n_Rra = ifelse(is.na(n_Rra), 0, n_Rra)
+    n_Rra = ifelse(is.na(n_Rra), 0, n_Rra),
+    n_Mer = ifelse(is.na(n_Mer), 0, n_Mer),
+    n_Pda = ifelse(is.na(n_Pda), 0, n_Pda),
+    n_Pro = ifelse(is.na(n_Pro), 0, n_Pro)
   )
 
 sum(track.level.captures$n_Mna)
 sum(track.level.captures$n_Rra)
+sum(track.level.captures$n_Mer)
+sum(track.level.captures$n_Pda)
+sum(track.level.captures$n_Pro)
 
 
-# Import combined site-level data
-site.dat <- read_csv(
-  "data/clean/combined/site_level_data.csv"
+# Import combined visit-level data
+visit.dat <- read_csv(
+  "data/clean/combined/visit_level_data.csv"
 )
 
 #==============================================================================
@@ -73,9 +102,11 @@ house.level.captures <- track.level.captures %>%
   ) %>%
   left_join(
     .,
-    site.dat %>%
-      select(site, Rra_at_site),
-    by = "site"
+    visit.dat %>%
+      filter(data_source == "PREEMPT") %>%
+      select(site, visit, date, Rra_at_site, wet_season) %>%
+      mutate(visit = as.numeric(visit)),
+    by = c("site", "visit", "date")
   )
 
 # What is average house-level catch per trap?
@@ -140,6 +171,7 @@ stan.dat <- list(
   n_Rra_s = standardize(house.level.captures$n_Rra),
   Rra_at_site = house.level.captures$Rra_at_site,
   Rra_at_house = house.level.captures$Rra_at_house,
+  wet_season = house.level.captures$wet_season,
   tot_traps = house.level.captures$tot_traps,
   log_tot_traps = log(house.level.captures$tot_traps),
   N_site = n_distinct(house.level.captures$site),
@@ -171,7 +203,7 @@ fit.m1$print(max_rows = 100)
 
 draws.m1 <- fit.m1$draws(format = "matrix") %>%
   data.frame() %>%
-  select(a, bR, sigma_site, sigma_house)
+  select(a, bR, bW, sigma_site, sigma_house)
 
 jpeg("outputs/misc/model_out_house_level_Rra_at_site.jpeg",
      width = 1000, height = 500, units = "px")
@@ -186,6 +218,42 @@ dev.off()
 # What portion of the posterior probability mass for bR is < 0?
 sum(draws.m1$bR < 0)/length(draws.m1$bR)
 
+
+# Generate parameter trace plots
+
+# Set palette for plotting
+palette <- adjustcolor(wesanderson::wes_palette("Darjeeling1"), alpha.f = 0.3)
+
+# Base figure
+p <- bayesplot::mcmc_trace(
+  fit.m1$draws(format = "matrix"), 
+  pars = c("a", "bR", "bW", "sigma_site", "sigma_house"),
+  size = 0.8,
+  facet_args = list(ncol = 2)
+) 
+
+# Relabel strip text
+levels(p$data$parameter) <- c(
+  "grand mean", "*Rattus rattus* effect (present vs. absent)",
+  "season effect (wet vs. dry)", "σ (for site-level random effects)",
+  "σ (for house-level random effects)"
+)
+
+# Plot
+p + 
+  scale_color_manual(values = palette) + 
+  theme_minimal() +
+  theme(
+    text = element_text(size = 20),
+    strip.text.x = ggtext::element_markdown(face = "bold")
+  )
+
+ggsave(
+  "outputs/misc/house_level_house_traps_Rra_at_site_trace_plots.jpeg",
+  width = 3500, height = 4000, units = "px"
+)
+
+
 # Generate figure of the Rra_at_site posterior (have to do some of this 
 # manually)
 cutoff <- 0
@@ -197,7 +265,7 @@ hist %>%
   ggplot(aes(x = x, ymin = 0, ymax = y, fill = area)) +
   geom_ribbon() +
   geom_line(aes(y = y), linewidth = 1) +
-  geom_vline(xintercept = 0, lty = 2, size = 2) +
+  geom_vline(xintercept = 0, lty = 2, linewidth = 2) +
   xlab(expression(paste("Coefficient for ", italic("Rattus rattus"), " presence"))) +
   ylab("Density") +
   xlim(-4, 2) +
@@ -238,7 +306,7 @@ fit.m2$print(max_rows = 100)
 
 draws.m2 <- fit.m2$draws(format = "matrix") %>%
   data.frame() %>%
-  select(a, bR, sigma_site, sigma_house)
+  select(a, bR, bW, sigma_site, sigma_house)
 
 jpeg("outputs/misc/model_out_house_level_Rra_at_house.jpeg",
      width = 1000, height = 500, units = "px")
@@ -253,6 +321,42 @@ dev.off()
 # What portion of the posterior probability mass for bR is < 0?
 sum(draws.m2$bR < 0)/length(draws.m2$bR)
 
+
+# Generate parameter trace plots
+
+# Set palette for plotting
+palette <- adjustcolor(wesanderson::wes_palette("Darjeeling1"), alpha.f = 0.3)
+
+# Base figure
+p <- bayesplot::mcmc_trace(
+  fit.m2$draws(format = "matrix"), 
+  pars = c("a", "bR", "bW", "sigma_site", "sigma_house"),
+  size = 0.8,
+  facet_args = list(ncol = 2)
+) 
+
+# Relabel strip text
+levels(p$data$parameter) <- c(
+  "grand mean", "*Rattus rattus* effect (present vs. absent)",
+  "season effect (wet vs. dry)", "σ (for site-level random effects)",
+  "σ (for house-level random effects)"
+)
+
+# Plot
+p + 
+  scale_color_manual(values = palette) + 
+  theme_minimal() +
+  theme(
+    text = element_text(size = 20),
+    strip.text.x = ggtext::element_markdown(face = "bold")
+  )
+
+ggsave(
+  "outputs/misc/house_level_house_traps_Rra_at_house_trace_plots.jpeg",
+  width = 3500, height = 4000, units = "px"
+)
+
+
 # Generate figure of the Rra_at_site posterior (have to do some of this 
 # manually)
 cutoff <- 0
@@ -263,8 +367,8 @@ hist <- data.frame(x = hist$x, y = hist$y) %>%
 hist %>%
   ggplot(aes(x = x, ymin = 0, ymax = y, fill = area)) +
   geom_ribbon() +
-  geom_line(aes(y = y), size = 1) +
-  geom_vline(xintercept = 0, lty = 2, size = 2) +
+  geom_line(aes(y = y), linewidth = 1) +
+  geom_vline(xintercept = 0, lty = 2, linewidth = 2) +
   xlab(expression(paste("Coefficient for ", italic("Rattus rattus"), " presence"))) +
   ylab("Density") +
   xlim(-4, 2) +
@@ -280,69 +384,6 @@ ggsave("outputs/misc/house_level_house_traps_Rra_at_house_coefficient.jpeg",
        width = 2000, height = 1600, units = "px")
 
 #==============================================================================
-
-
-# Compute site-level predictions from model output (not really predictions, just
-# looking at the lambda values by site)
-
-# Generate predictions for each site equal in number to the total number of
-# model iterations
-
-site.df <- house.level.captures %>%
-  distinct(site, Rra_at_site)
-
-preds <- data.frame(
-  site = rep(site.df$site, each = nrow(draws.m1)),
-  Rra_at_site = rep(site.df$Rra_at_site, each = nrow(draws.m1)),
-  a = rep(draws.m1$a, times = n_distinct(house.level.captures$site)),
-  bR = rep(draws.m1$bR, times = n_distinct(house.level.captures$site))
-)
-  
-varying.effect.df <- fit.m1$draws(format = "matrix") %>%
-  data.frame() %>%
-  select(contains("a_site."))
-str(varying.effect.df)
-apply(varying.effect.df, 2, mean)
-
-temp <- c()
-for(i in 1:length(site.df$site)) {
-  
-  temp <- c(temp, varying.effect.df[ , i])
-}
-
-preds$a_site <- temp
-
-preds <- preds %>%
-  mutate(
-    lambda = exp(a + bR * Rra_at_site + a_site)
-  )
-
-preds.summary <- preds %>%
-  group_by(site) %>%
-  summarize(
-    lower90 = HPDI(lambda, 0.9)[1],
-    upper90 = HPDI(lambda, 0.9)[2],
-    lower99 = HPDI(lambda, 0.99)[1],
-    upper99 = HPDI(lambda, 0.99)[2],
-  ) %>%
-  ungroup()
-
-ggplot() +
-  geom_linerange(
-    aes(x = 1:nrow(preds.summary), ymin = lower99, ymax = upper99),
-    data = preds.summary, size = 1
-  ) +
-  geom_linerange(
-    aes(x = 1:nrow(preds.summary), ymin = lower90, ymax = upper90),
-    data = preds.summary, size = 2
-  ) +
-  geom_jitter(
-    aes(x = as.numeric(as.factor(site)), y = Mna_per_trap, color = as.factor(Rra_at_site)),
-    data = house.level.captures,
-    height = 0, width = 0.25
-  ) +
-  scale_color_manual(values = c(alpha("black", 0.2), alpha("darkred", 0.2))) +
-  theme_minimal()
 
 
 # Compute house-level predictions from model output
@@ -382,11 +423,11 @@ sum(preds.summary$in_interval)
 ggplot() +
   geom_linerange(
     aes(x = 1:nrow(house.level.captures), ymin = lower99, ymax = upper99, color = site),
-    data = preds.summary, size = 1
+    data = preds.summary, linewidth = 1
   ) +
   geom_linerange(
     aes(x = 1:nrow(house.level.captures), ymin = lower90, ymax = upper90, color = site),
-    data = preds.summary, size = 2
+    data = preds.summary, linewidth = 2
   ) +
   geom_point(
     aes(x = 1:nrow(house.level.captures), y = n_Mna, col = as.factor(Rra_at_site)), 
@@ -470,20 +511,20 @@ o2 <- o %>%
   left_join(
     .,
     house.level.captures %>%
-      select(house_id, Rra_at_site),
+      select(house_id, Rra_at_site, wet_season),
     by = "house_id"
   )
 
 # Pivot longer to get all trapping occasions from a single house on one row
 o3 <- o2 %>%
   pivot_wider(
-    id_cols = c(site, visit, track_id, house_id, Rra_at_site),
+    id_cols = c(site, visit, track_id, house_id, Rra_at_site, wet_season),
     names_from = "night",
     values_from = "Mna_detected"
   )
 
 # If there are 3 NAs in a given row it means we only have one observation: drop
-good.indices <- which(rowSums(is.na(o3[, 6:9])) < 3)
+good.indices <- which(rowSums(is.na(o3[, 7:10])) < 3)
 o3 <- slice(o3, good.indices)
 
 # Generate occupancy covariates
@@ -491,7 +532,8 @@ occ.covs <- data.frame(
   site = o3$site,
   site_numeric = as.numeric(as.factor(o3$site)),
   house_numeric = as.numeric(as.factor(o3$house_id)),
-  Rra_at_site = o3$Rra_at_site
+  Rra_at_site = o3$Rra_at_site,
+  wet_season = o3$wet_season
 )
 
 # Generate detection covariate: trap count
@@ -538,7 +580,7 @@ det.covs <- list(
 
 # Generate data list for model fitting
 data.list <- list(
-  y = o3[, 6:9],
+  y = o3[, 7:10],
   occ.covs = occ.covs,
   det.covs = det.covs
 )
@@ -563,32 +605,34 @@ o3 %>%
   pull(occupancy) %>%
   mean()
 
-# What is occupancy conditional on R. rattus presence (complete pooling by site)
+# What is occupancy conditional on R. rattus presence and season
+# (complete pooling by site)
 o3 %>%
   rowwise() %>%
   mutate(
     sum = sum(`1`, `2`, `3`, `4`, na.rm = T),
     occupied = sum > 0
   ) %>%
-  group_by(site, Rra_at_site) %>%
+  group_by(site, Rra_at_site, wet_season) %>%
   summarize(
     n = n(),
     occupied = sum(occupied),
     occupancy = occupied/n()
   ) %>%
-  group_by(Rra_at_site) %>%
+  group_by(Rra_at_site, wet_season) %>%
   summarize(
     occupancy = mean(occupancy)
   )
 
-# What is occupancy condition on R. rattus presence (no pooling by site)
+# What is occupancy condition on R. rattus presence and season
+# (no pooling by site)
 o3 %>%
   rowwise() %>%
   mutate(
     sum = sum(`1`, `2`, `3`, `4`, na.rm = T),
     occupied = sum > 0
   ) %>%
-  group_by(Rra_at_site) %>%
+  group_by(Rra_at_site, wet_season) %>%
   summarize(
     n = n(),
     n_occupied = sum(occupied),
@@ -604,7 +648,7 @@ priors.list <- list(
 
 # Fit an occupancy model
 out <- spOccupancy::PGOcc(
-  occ.formula = ~ Rra_at_site + (1|site_numeric) + (1|house_numeric), 
+  occ.formula = ~ Rra_at_site + wet_season + (1|site_numeric) + (1|house_numeric), 
   det.formula = ~ trap_count + cumulative_Mna_count, 
   data = data.list, 
   priors = priors.list,
@@ -627,6 +671,11 @@ dens(out$beta.samples[,2], show.HPDI = 0.9)
 
 # What portion of the posterior probability mass for bR is < 0?
 sum(out$beta.samples[,2] < 0)/length(out$beta.samples[,2])
+
+# Wet season sampling parameter
+mean(out$beta.samples[,3])
+HPDI(out$beta.samples[,3], prob = 0.99)
+dens(out$beta.samples[,3], show.HPDI = 0.9)
 
 # Baseline detection parameter
 mean(out$alpha.samples[,1])
@@ -709,6 +758,15 @@ for(site in unique(o3$site)) {
   site.preds <- bind_rows(site.preds, temp)
 }
 
+site.preds <- site.preds %>%
+  left_join(
+    .,
+    o3 %>%
+      select(site, Rra_at_site) %>%
+      distinct(site, Rra_at_site),
+    by = "site"
+  )
+
 site.preds %>%
   group_by(site) %>%
   summarize(
@@ -731,7 +789,7 @@ naive.occ <- o2 %>%
   )
 
 site.preds %>%
-  group_by(site) %>%
+  group_by(site, Rra_at_site) %>%
   summarize(
     mean_occ_prob = mean(occ_prob),
     median_occ_prob = median(occ_prob),
@@ -739,40 +797,53 @@ site.preds %>%
     upper = HPDI(occ_prob, 0.99)[2]
   ) %>%
   ungroup() %>%
-  ggplot(aes(x = site, y = median_occ_prob)) +
+  ggplot(aes(x = site, y = median_occ_prob, color = as.factor(Rra_at_site))) +
   geom_point(size = 6) +
   geom_linerange(aes(ymin = lower, ymax = upper)) +
   geom_hline(yintercept = mean(logistic(out$beta.samples[,1])), lty = 2) +
-  geom_hline(yintercept = mean(logistic(out$beta.samples[,1] + out$beta.samples[,2])), lty = 2, color = "red") +
+  geom_hline(yintercept = mean(logistic(out$beta.samples[,1] + out$beta.samples[,2])), lty = 2, color = "darkred") +
   geom_point(
     data = naive.occ, 
     aes(x = site, y = naive_occ, color = as.factor(Rra_at_site)), 
     pch = 1, size = 6) +
-  scale_color_manual(values = c("black", "red")) +
-  ylab("Mastomys natalensis occupancy") +
-  theme_minimal()
+  scale_color_manual(values = c("black", "darkred")) +
+  xlab("") +
+  ylab("Proportion of houses occupied by *Mastomys natalensis*") +
+  theme_minimal() +
+  theme(
+    text = element_text(size = 14),
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    axis.title.y = ggtext::element_markdown(),
+    legend.position = "none"
+  )
 
 ggsave("outputs/misc/observed_predicted_Mna_site_level_occupancy.jpeg",
        width = 3500, height = 1500, units = "px")
 
 
 preds <- data.frame(
-  site = rep(
-    c("Rattus rattus absent", "Rattus rattus present"),
-    each = length(out$beta.samples[,1])
+  season = rep(
+    c("Wet season", "Dry season"), 
+    each = length(out$beta.samples[,1])*2
   ),
   site_status = rep(
-    c("Absent", "Present"),
+    c("Absent", "Present",
+      "Absent", "Present"),
     each = length(out$beta.samples[,1])
   ),
-  beta = c(out$beta.samples[,1], (out$beta.samples[,1] + out$beta.samples[,2]))
+  beta = c(
+    out$beta.samples[,1] + out$beta.samples[,3], 
+    out$beta.samples[,1] + out$beta.samples[,3] + out$beta.samples[,2],
+    out$beta.samples[,1], 
+    out$beta.samples[,1] + out$beta.samples[,2]
+  )
 ) %>%
   mutate(
     occ_prob = logistic(beta)
   )
 
 preds %>%
-  group_by(site_status) %>%
+  group_by(site_status, season) %>%
   summarize(
     mean_occ_prob = mean(occ_prob),
     median_occ_prob = median(occ_prob),
@@ -786,8 +857,8 @@ preds %>%
 
 
 occ.plot <- preds %>%
-  ggplot(aes(x = occ_prob, color = site)) +
-  geom_density(aes(fill = site), size = 1) +
+  ggplot(aes(x = occ_prob, color = site_status)) +
+  geom_density(aes(fill = site_status), size = 1) +
   xlab(expression(paste("House-level occupancy of ", italic("Mastomys natalensis")))) +
   ylab("Density") +
   scale_color_manual(
@@ -808,7 +879,7 @@ occ.plot <- preds %>%
   xlim(0, 1) +
   geom_vline(
     xintercept = preds %>%
-      filter(site == "Rattus rattus absent") %>%
+      filter(site_status == "Absent") %>%
       pull(occ_prob) %>%
       median(),
     color = "black",
@@ -816,7 +887,7 @@ occ.plot <- preds %>%
   ) +
   geom_vline(
     xintercept = preds %>%
-      filter(site == "Rattus rattus present") %>%
+      filter(site_status == "Present") %>%
       pull(occ_prob) %>%
       median(),
     color = "darkred",
@@ -828,8 +899,10 @@ occ.plot <- preds %>%
     legend.title = element_blank()
   )
 
+val.dodge <- 0.25
+
 occ.plot <- preds %>%
-  group_by(site_status) %>%
+  group_by(season, site_status) %>%
   summarize(
     mean = mean(occ_prob),
     lower99 = HPDI(occ_prob, 0.99)[1],
@@ -838,15 +911,23 @@ occ.plot <- preds %>%
     upper90 = HPDI(occ_prob, 0.9)[2]
   ) %>%
   ungroup() %>%
-  ggplot(aes(x = site_status, y = mean, color = site_status)) +
-  geom_linerange(aes(ymin = lower99, ymax = upper99), size = 1) +
-  geom_linerange(aes(ymin = lower90, ymax = upper90), size = 3) +
-  geom_point(size = 5) +
+  ggplot(aes(x = site_status, y = mean, color = season, group = season)) +
+  geom_linerange(
+    aes(ymin = lower99, ymax = upper99), 
+    position = position_dodge2(width = val.dodge),
+    linewidth = 1
+  ) +
+  geom_linerange(
+    aes(ymin = lower90, ymax = upper90), 
+    position = position_dodge2(width = val.dodge),
+    linewidth = 3
+  ) +
+  geom_point(position = position_dodge2(width = val.dodge), size = 5) +
   xlab(expression(paste(italic("Rattus rattus"), " status at site"))) +
   ylab(expression(atop(italic("Mastomys natalensis"), "house-level occupancy"))) +
   scale_y_continuous(limits = c(0, 0.6)) +
   theme_minimal() +
-  scale_color_manual(values = c("darkgrey", "darkred")) +
+  scale_color_manual(values = c("wheat3", "steelblue")) +
   theme(
     text = element_text(size = 21),
     legend.position = "none"
--- stan_models/house_model_Rra_at_site.stan
+++ stan_models/house_model_Rra_at_site.stan
@@ -1,5 +1,5 @@
-// A Poisson regression with effect of Rra_at_site and varying intercepts 
-// by site and house, to be used with house-level data
+// A Poisson regression with effect of Rra_at_site, wet season, and
+// varying intercepts by site and house, to be used with house-level data
 
 data {
   
@@ -7,6 +7,7 @@ data {
   
   array[N] int n_Mna;
   array[N] int Rra_at_site;
+  array[N] int wet_season;
   array[N] real log_tot_traps;
   
   int<lower=0> N_site;
@@ -20,6 +21,7 @@ parameters {
   
   real a; // intercept
   real bR; // Rattus effect
+  real bW; // wet season effect
   
   // varying intercepts by site (centered parameterization)
   vector[N_site] a_site_z;
@@ -38,6 +40,7 @@ model {
   
   a ~ normal(-3.1, 1.1);
   bR ~ normal(0, 1);
+  bW ~ normal(0, 1);
   
   a_site_z ~ normal(0, 1);
   sigma_site ~ exponential(1);
@@ -57,7 +60,8 @@ model {
   
   for (i in 1:N) {
     
-    lambda[i] = a + bR * Rra_at_site[i] + a_site[site[i]] + a_house[house[i]] + log_tot_traps[i];
+    lambda[i] = a + bR * Rra_at_site[i] + bW * wet_season[i] + 
+    a_site[site[i]] + a_house[house[i]] + log_tot_traps[i];
     n_Mna[i] ~ poisson_log(lambda[i]);
   }
 }
@@ -80,6 +84,7 @@ generated quantities {
   
   for (i in 1:N) {
     
-    lambda[i] = a + bR * Rra_at_site[i] + a_site[site[i]] + a_house[house[i]];
+    lambda[i] = a + bR * Rra_at_site[i] + bW * wet_season[i] + 
+    a_site[site[i]] + a_house[house[i]];
   }
 }
