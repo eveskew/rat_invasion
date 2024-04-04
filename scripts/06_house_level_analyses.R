a5e7d45fce53ef8bd3d95500798cbde9e875ef4a
Evan Eskew <eveskew@gmail.com>
2024-04-04 15:44:47 -0700

    Rerunning house-level analysis script

--- scripts/06_house_level_analyses.R
+++ scripts/06_house_level_analyses.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 library(rethinking)
-library(lubridate)
 library(assertthat)
 
 source("R/functions.R")
@@ -8,116 +7,10 @@ source("R/functions.R")
 #==============================================================================
 
 
-# Import cleaned trapping and capture data files from Sierra Leone (the 
-# PREEMPT project)
-
-# Data on track-level trapping effort across all sites
-summary.trap.dat <- read_csv("data/clean/PREEMPT/trap_metadata.csv") %>%
-  mutate(
-    # visit 5 at Petema is coded with two dates two days apart, recode to have
-    # the same visit date
-    date_mod = ifelse(
-      site == "Petema" & as.character(date) == "2021-01-28",
-      "2021-01-26",
-      as.character(date)
-    ),
-    # visit 2 at Badala is coded in the incorrect year, recode
-    date_mod = ifelse(
-      site == "Badala" & visit == 2,
-      "2019-11-09",
-      date_mod
-    ),
-    date_mod = as.Date(date_mod)
-  ) %>%
-  select(-mastomys, -house_type, -date) %>%
-  rename(date = date_mod) %>%
-  relocate(date, everything())
-
-# Data on rodent captures across all sites
-capture.dat <- read_csv("data/clean/PREEMPT/PREEMPT_capture_data.csv")
-
-
-# Generate track-level data tables for Sierra Leone
-
-# Summarize capture data by site, visit, and track ID
-capture.dat.by.track <- capture.dat %>%
-  group_by(site, visit, track_id) %>%
-  summarize(
-    n_catch = n(),
-    n_Mna = sum(sp == "Mna"),
-    n_Rra = sum(sp == "Rra"),
-    n_Mer = sum(sp == "Mer"),
-    n_Pda = sum(sp == "Pda"),
-    n_Pro = sum(sp == "Pro")
-  ) %>%
-  ungroup()
-
-sum(capture.dat.by.track$n_Mna)
-sum(capture.dat.by.track$n_Rra)
-sum(capture.dat.by.track$n_Mer)
-sum(capture.dat.by.track$n_Pda)
-sum(capture.dat.by.track$n_Pro)
-
-# Join summarized capture data in with track-level trap data
-track.level.captures <- summary.trap.dat %>%
-  left_join(
-    ., capture.dat.by.track,
-    by = c("site", "visit", "track_id")
-  ) %>%
-  mutate(
-    n_Mna = ifelse(is.na(n_Mna), 0, n_Mna),
-    n_Rra = ifelse(is.na(n_Rra), 0, n_Rra),
-    n_Mer = ifelse(is.na(n_Mer), 0, n_Mer),
-    n_Pda = ifelse(is.na(n_Pda), 0, n_Pda),
-    n_Pro = ifelse(is.na(n_Pro), 0, n_Pro)
-  )
-
-sum(track.level.captures$n_Mna)
-sum(track.level.captures$n_Rra)
-sum(track.level.captures$n_Mer)
-sum(track.level.captures$n_Pda)
-sum(track.level.captures$n_Pro)
-
-
-# Import combined visit-level data
-visit.dat <- read_csv(
-  "data/clean/combined/visit_level_data.csv"
-)
-
-#==============================================================================
-
-
 # House-level analyses
 
-# Generate house-level data frame
-house.level.captures <- track.level.captures %>%
-  filter(habitat_code == "H") %>%
-  # Remove zero trapping effort houses!
-  filter(tot_traps != 0) %>%
-  mutate(
-    house_id = paste(site, visit, track_id, sep = "-"),
-    house_w_only_Mna = n_Mna > 0 & n_Rra == 0,
-    house_w_only_Rra = n_Mna == 0 & n_Rra > 0,
-    house_w_both = n_Mna > 0 & n_Rra > 0,
-    house_w_neither = n_Mna == 0 & n_Rra == 0,
-    Mna_per_trap = n_Mna/tot_traps,
-    Rra_at_house = ifelse(n_Rra > 0, 1, 0),
-    Mer_at_house = ifelse(n_Mer > 0, 1, 0),
-    Pda_at_house = ifelse(n_Pda > 0, 1, 0),
-    Pro_at_house = ifelse(n_Pro > 0, 1, 0)
-  ) %>%
-  left_join(
-    .,
-    visit.dat %>%
-      filter(data_source == "PREEMPT") %>%
-      select(
-        site, visit, date, 
-        Rra_at_site, Mer_at_site, Pda_at_site, Pro_at_site,
-        wet_season
-      ) %>%
-      mutate(visit = as.numeric(visit)),
-    by = c("site", "visit", "date")
-  )
+# Import house-level trapping data frame from Sierra Leone
+house.level.captures <- read_csv("data/clean/house/house_level_data.csv")
 
 # What is average house-level catch per trap?
 house.level.captures %>%
@@ -157,32 +50,6 @@ house.yes.no <- house.level.captures %>%
   ggplot(aes(x = Rra_at_site_character, y = Mna_per_trap)) +
   geom_violin(fill = alpha("lightgrey", 0.3), linewidth = 0.2) +
   geom_jitter(aes(color = Rra_at_site_character), height = 0, width = 0.25, size = 5) +
-  # geom_segment(
-  #   x = 0.75, xend = 1.25,
-  #   y = house.level.captures %>%
-  #     filter(Rra_at_site == 0) %>%
-  #     pull(Mna_per_trap) %>%
-  #     mean(),
-  #   yend = house.level.captures %>%
-  #     filter(Rra_at_site == 0) %>%
-  #     pull(Mna_per_trap) %>%
-  #     mean(),
-  #   color = "black",
-  #   linewidth = 2
-  # ) +
-  # geom_segment(
-  #   x = 1.75, xend = 2.25,
-  #   y = house.level.captures %>%
-  #     filter(Rra_at_site == 1) %>%
-  #     pull(Mna_per_trap) %>%
-  #     mean(),
-  #   yend = house.level.captures %>%
-  #     filter(Rra_at_site == 1) %>%
-  #     pull(Mna_per_trap) %>%
-  #     mean(),
-  #   color = "darkred",
-  #   linewidth = 2
-  # ) +
   xlab(expression(paste(italic("Rattus rattus"), " status at site"))) +
   ylab(expression(atop(italic("Mastomys natalensis"), "catch per trap"))) +
   scale_color_manual(values = c(alpha("black", 0.15), alpha("darkred", 0.15))) +
@@ -390,7 +257,7 @@ p +
   )
 
 ggsave(
-  "outputs/misc/house_level_house_traps_Rra_at_site_trace_plots.jpeg",
+  "outputs/supplementary/house_level_house_traps_Rra_at_site_trace_plots.jpeg",
   width = 3500, height = 4000, units = "px"
 )
 
@@ -494,7 +361,7 @@ p +
   )
 
 ggsave(
-  "outputs/misc/house_level_house_traps_Rra_at_house_trace_plots.jpeg",
+  "outputs/supplementary/house_level_house_traps_Rra_at_house_trace_plots.jpeg",
   width = 3500, height = 4000, units = "px"
 )
 
@@ -719,7 +586,7 @@ dat.ridges %>%
   )
 
 ggsave(
-  "outputs/misc/species_presence_effect_posteriors_house_model_site_predictors.jpeg", 
+  "outputs/supplementary/species_presence_effect_posteriors_house_model_site_predictors.jpeg", 
   width = 3000, 
   height = 2000, 
   unit = "px"
@@ -847,7 +714,7 @@ dat.ridges %>%
   )
 
 ggsave(
-  "outputs/misc/species_presence_effect_posteriors_house_model_house_predictors.jpeg", 
+  "outputs/supplementary/species_presence_effect_posteriors_house_model_house_predictors.jpeg", 
   width = 3000, 
   height = 2000, 
   unit = "px"
@@ -856,147 +723,25 @@ ggsave(
 #==============================================================================
 
 
-# Occupancy analyses - data preparation
+# Occupancy analyses - data importation
 
-o <- summary.trap.dat %>%
-  # get rid of no-house information
-  filter(habitat_code == "H") %>%
-  # create total trap counts by night and house IDs
-  mutate(
-    date = as.Date(date),
-    n1_traps = n1_traps - n1_ce - n1_miss,
-    n2_traps = n2_traps - n2_ce - n2_miss,
-    n3_traps = n3_traps - n3_ce - n3_miss,
-    n4_traps = n4_traps - n4_ce - n4_miss,
-    house_id = paste(site, visit, track_id, sep = "-")
-  ) %>%
-  # select only a subset of relevant columns
-  select(
-    date, site, visit, track_id, house_id,
-    n1_traps, n2_traps, n3_traps, n4_traps
-  ) %>%
-  # pivot longer to get each row as a night of trapping at a given house
-  pivot_longer(
-    cols = starts_with("n"),
-    names_to = "night",
-    values_to = "trap_count"
-  ) %>%
-  # eliminate any nights without traps out
-  filter(
-    trap_count > 0,
-    !is.na(trap_count)
-  ) %>%
-  # clean up the "night" variable and "date" column to actually correspond 
-  # with each night of trapping
-  mutate(
-    night = as.numeric(as.vector(str_match(night, "[0-9]"))),
-    date = case_when(
-      night == 1 ~ date,
-      night == 2 ~ date + days(1),
-      night == 3 ~ date + days(2),
-      night == 4 ~ date + days(3)
-    )
-  )
+# Import Mastomys natalensis detection data
+o <- read_csv("data/clean/occupancy/Mna_detection.csv")
 
-# Add on info about rodent catch for each of these trapping occasions
-o2 <- o %>%
-  left_join(
-    .,
-    capture.dat %>%
-      filter(habitat_code == "H") %>%
-      select(site, visit, track_id, night, species),
-    by = c("site", "visit", "track_id", "night")
-  ) %>%
-  group_by(date, site, visit, track_id, house_id, night, trap_count) %>%
-  summarize(
-    n_rodents = sum(!is.na(species)),
-    n_Mna = sum(species == "Mastomys natalensis"),
-    n_Rra = sum(species == "Rattus rattus")
-  ) %>%
-  ungroup() %>%
-  mutate(
-    n_Mna = ifelse(is.na(n_Mna), 0, n_Mna),
-    n_Rra = ifelse(is.na(n_Rra), 0, n_Rra),
-    Mna_detected = ifelse(n_Mna > 0, 1, 0),
-    Rra_detected = ifelse(n_Rra > 0, 1, 0)
-  ) %>%
-  arrange(site, visit, house_id, night) %>%
-  left_join(
-    .,
-    house.level.captures %>%
-      select(house_id, Rra_at_site, wet_season),
-    by = "house_id"
-  )
-
-# Pivot longer to get all trapping occasions from a single house on one row
-o3 <- o2 %>%
-  pivot_wider(
-    id_cols = c(site, visit, track_id, house_id, Rra_at_site, wet_season),
-    names_from = "night",
-    values_from = "Mna_detected"
-  )
-
-# If there are 3 NAs in a given row it means we only have one observation: drop
-good.indices <- which(rowSums(is.na(o3[, 7:10])) < 3)
-o3 <- slice(o3, good.indices)
-
-# Generate occupancy covariates
-occ.covs <- data.frame(
-  site = o3$site,
-  site_numeric = as.numeric(as.factor(o3$site)),
-  visit_numeric = paste0(o3$site, "-", o3$visit) %>%
-    as.factor() %>%
-    as.numeric(),
-  house_numeric = as.numeric(as.factor(o3$house_id)),
-  Rra_at_site = o3$Rra_at_site,
-  wet_season = o3$wet_season
-)
-
-# Generate detection covariate: trap count
-det.covs.trap.count <- o2 %>%
-  pivot_wider(
-    id_cols = house_id,
-    names_from = "night",
-    values_from = "trap_count"
-  ) %>%
-  slice(good.indices) %>%
-  select(-house_id)
-
-# Generate detection covariate: cumulative Mna catch
-temp.cumulative.counts <- o2 %>%
-  group_by(house_id) %>%
-  mutate(cumulative_Mna_count = cumsum(n_Mna)) %>%
-  select(site, visit, house_id, night, cumulative_Mna_count)
-
-det.covs.cumulative.Mna.count <- o2 %>%
-  left_join(
-    .,
-    temp.cumulative.counts %>%
-      mutate(night = night + 1),
-    by = c("site", "visit", "house_id", "night")
-  ) %>%
-  group_by(house_id) %>%
-  # for every house, the first trapping occasion has 0 cumulative catch
-  mutate(cumulative_Mna_count = replace(cumulative_Mna_count, 1, 0)) %>%
-  ungroup() %>%
-  pivot_wider(
-    id_cols = house_id,
-    names_from = "night",
-    values_from = "cumulative_Mna_count"
-  ) %>%
-  ungroup() %>%
-  slice(good.indices) %>%
-  select(-house_id)
+# Import occupancy and detection covariate data
+occ.covs <- read_csv("data/clean/occupancy/occupancy_covariates.csv")
+det.cov.trap.count <- read_csv("data/clean/occupancy/detection_trap_count_covariate.csv")
+det.cov.cumulative.Mna.count <- read_csv("data/clean/occupancy/detection_cumulative_Mna_count_covariate.csv")
 
 # Generate detection covariate list
 det.covs <- list(
-  trap_count = det.covs.trap.count,
-  cumulative_Mna_count = det.covs.cumulative.Mna.count
+  trap_count = det.cov.trap.count,
+  cumulative_Mna_count = det.cov.cumulative.Mna.count
 )
 
 # Generate data list for model fitting
 data.list <- list(
-  y = o3[, 7:10],
+  y = o[, 7:10],
   occ.covs = occ.covs,
   det.covs = det.covs
 )
@@ -1007,7 +752,7 @@ data.list <- list(
 # Occupancy analyses - model fitting and interpretation
 
 # What is average site-level occupancy?
-o3 %>%
+o %>%
   rowwise() %>%
   mutate(
     sum = sum(`1`, `2`, `3`, `4`, na.rm = T),
@@ -1023,7 +768,7 @@ o3 %>%
 
 # What is occupancy conditional on R. rattus presence and season
 # (complete pooling by site)
-o3 %>%
+o %>%
   rowwise() %>%
   mutate(
     sum = sum(`1`, `2`, `3`, `4`, na.rm = T),
@@ -1042,7 +787,7 @@ o3 %>%
 
 # What is occupancy conditional on R. rattus presence and season
 # (no pooling by site)
-o3 %>%
+o %>%
   rowwise() %>%
   mutate(
     sum = sum(`1`, `2`, `3`, `4`, na.rm = T),
@@ -1181,9 +926,9 @@ site.preds <- data.frame(
   occ_prob = double()
 )
 
-for(site in unique(o3$site)) {
+for(site in unique(o$site)) {
   
-  occ_prob <- apply(out$z.samples[ , which(o3$site == site)], 1, sum)/length(which(o3$site == site))
+  occ_prob <- apply(out$z.samples[ , which(o$site == site)], 1, sum)/length(which(o$site == site))
   temp <- data.frame(
     site = rep(as.character(site), length(occ_prob)),
     occ_prob
@@ -1195,7 +940,7 @@ for(site in unique(o3$site)) {
 site.preds <- site.preds %>%
   left_join(
     .,
-    o3 %>%
+    o %>%
       select(site, Rra_at_site) %>%
       distinct(site, Rra_at_site),
     by = "site"
@@ -1210,10 +955,9 @@ site.preds %>%
     upper = HPDI(occ_prob, 0.99)[2]
   )
 
-naive.occ <- o2 %>%
-  group_by(site, house_id, Rra_at_site) %>%
-  summarize(
-    house_w_Mna = ifelse(sum(n_Mna) > 0, 1, 0)
+naive.occ <- o %>%
+  mutate(
+    house_w_Mna = ifelse(rowSums(.[,7:10], na.rm = TRUE) > 0, 1, 0)
   ) %>%
   group_by(site, Rra_at_site) %>%
   summarize(
@@ -1371,7 +1115,8 @@ occ.plot <- preds %>%
     text = element_text(size = 21),
     legend.title = element_blank(),
     legend.text = element_text(size = 12),
-    legend.position = c(0.7, 0.82),
+    legend.position = "inside",
+    legend.position.inside = c(0.7, 0.82),
     legend.background = element_rect(fill = "white", colour = 0)
   )
 
@@ -1394,33 +1139,7 @@ house.yes.no <- house.level.captures %>%
   ggplot(aes(x = Rra_at_site_character, y = Mna_per_trap)) +
   geom_violin(fill = alpha("lightgrey", 0.3), linewidth = 0.2/2) +
   geom_jitter(aes(color = Rra_at_site_character), height = 0, width = 0.25, size = 5/2) +
-  # geom_segment(
-  #   x = 0.75, xend = 1.25,
-  #   y = house.level.captures %>%
-  #     filter(Rra_at_site == 0) %>%
-  #     pull(Mna_per_trap) %>%
-  #     mean(),
-  #   yend = house.level.captures %>%
-  #     filter(Rra_at_site == 0) %>%
-  #     pull(Mna_per_trap) %>%
-  #     mean(),
-  #   color = "black",
-#   linewidth = 2
-# ) +
-# geom_segment(
-#   x = 1.75, xend = 2.25,
-#   y = house.level.captures %>%
-#     filter(Rra_at_site == 1) %>%
-#     pull(Mna_per_trap) %>%
-#     mean(),
-#   yend = house.level.captures %>%
-#     filter(Rra_at_site == 1) %>%
-#     pull(Mna_per_trap) %>%
-#     mean(),
-#   color = "darkred",
-#   linewidth = 2
-# ) +
-xlab(expression(paste(italic("Rattus rattus"), " status at site"))) +
+  xlab(expression(paste(italic("Rattus rattus"), " status at site"))) +
   ylab(expression(atop(italic("Mastomys natalensis"), "catch per trap"))) +
   scale_color_manual(values = c(alpha("black", 0.15), alpha("darkred", 0.15))) +
   theme_minimal() +
@@ -1461,7 +1180,8 @@ occ.plot <- preds %>%
     text = element_text(size = 21/2),
     legend.title = element_blank(),
     legend.text = element_text(size = 12/2),
-    legend.position = c(0.7, 0.82),
+    legend.position = "inside",
+    legend.position.inside = c(0.7, 0.82),
     legend.background = element_rect(fill = "white", colour = 0)
   )
 
